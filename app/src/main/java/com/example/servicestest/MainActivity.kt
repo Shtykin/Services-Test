6838ad867e6c53748421d199cf49fd09910d9a5f
Shtykin <shtykin@mail.ru>
2023-01-09 16:11:36 +0700

    added enqueue

--- app/src/main/java/com/example/servicestest/MainActivity.kt
+++ app/src/main/java/com/example/servicestest/MainActivity.kt
@@ -5,6 +5,7 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.job.JobInfo
 import android.app.job.JobScheduler
+import android.app.job.JobWorkItem
 import android.content.ComponentName
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -14,6 +15,8 @@ import androidx.core.content.ContextCompat
 import com.example.servicestest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var page: Int = 0
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
@@ -43,11 +46,17 @@ class MainActivity : AppCompatActivity() {
             val jobInfo = JobInfo.Builder(MyJobService.JOB_ID, componentName)
                 //.setRequiresCharging(true)                                  //работа только при зарядке
                 .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)     //только при wifi
-                .setPersisted(true)                             //запуск после выключения устройства
+                //.setPersisted(true)                             //запуск после выключения устройства
                 .build()
 
             val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
-            jobScheduler.schedule(jobInfo)
+            //jobScheduler.schedule(jobInfo)
+
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val intent = MyJobService.newIntent(page++)
+                jobScheduler.enqueue(jobInfo, JobWorkItem(intent))
+            }
         }
     }
 }
--- app/src/main/java/com/example/servicestest/MyJobService.kt
+++ app/src/main/java/com/example/servicestest/MyJobService.kt
@@ -5,6 +5,7 @@ import android.app.job.JobParameters
 import android.app.job.JobService
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
 import kotlinx.coroutines.*
@@ -19,12 +20,21 @@ class MyJobService : JobService() {
     }
 
     override fun onStartJob(params: JobParameters?): Boolean {
-        coroutineScope.launch {
-            for (i in 1..100) {
-                log("Counter: $i")
-                delay(1000)
+        log("onStartJob")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            coroutineScope.launch {
+                var workItem = params?.dequeueWork()
+                while (workItem != null) {
+                    val page = workItem.intent.getIntExtra(PAGE, 0)
+                    for (i in 1..5) {
+                        log("Page: $page Counter: $i")
+                        delay(1000)
+                    }
+                    params?.completeWork(workItem)
+                    workItem = params?.dequeueWork()
+                }
+                jobFinished(params, true)
             }
-            jobFinished(params, true)
         }
         return true
     }
@@ -44,7 +54,14 @@ class MyJobService : JobService() {
         Log.d("DEBUG", "MyJobService: $message")
     }
 
-    companion object{
+    companion object {
         const val JOB_ID = 111
+        private const val PAGE = "page"
+
+        fun newIntent(page: Int): Intent {
+            return Intent().apply {
+                putExtra(PAGE, page)
+            }
+        }
     }
 }
